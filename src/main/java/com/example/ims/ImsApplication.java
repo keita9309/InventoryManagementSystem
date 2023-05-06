21d7d7797727c2f22e7f0c08d53837d49118c23f
Keita <herausforderer09@gmail.com>
2023-05-06 14:27:09 +0900

    jarファイル/warファイル作成のための設定追加 [修正] ・build.gradle ・ImsApplication.java

--- src/main/java/com/example/ims/ImsApplication.java
+++ src/main/java/com/example/ims/ImsApplication.java
@@ -2,12 +2,22 @@ package com.example.ims;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class ImsApplication {
+@EnableScheduling
+public class ImsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ImsApplication.class, args);
+	}
+
+	// warファイルを作成するためのメソッド
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ImsApplication.class);
 	}
 
 }
